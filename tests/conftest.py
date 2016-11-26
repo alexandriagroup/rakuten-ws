98d1def196f6deb1002b7f2fb4483116f2891baa
Salem Harrache <salem.harrache@abc-culture.fr>
2016-11-26 18:17:45 +0100

    Added use_vcr pytest fixture to record online http interactions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,68 @@
 # coding: utf-8
-from __future__ import unicode_literals, print_function
+from __future__ import unicode_literals, division, absolute_import
+
 
 import os
+import sys
 
+import mock
 import pytest
 
+from vcr import VCR
 
-ONLINE_TEST_ENABLED = os.environ.get("RAKUTEN_APP_ID", None) is not None
+
+VCR_CASSETTE_DIR = os.path.join(os.path.dirname(__file__), 'cassettes')
+VCR_RECORD_MODE = os.environ.get('VCR_RECORD_MODE', 'once')
 
 
-@pytest.fixture(params=[pytest.mark.skipif(not ONLINE_TEST_ENABLED,
-                        reason='need credentials')('parameter')])
+vcr = VCR(
+    cassette_library_dir=VCR_CASSETTE_DIR,
+    record_mode=VCR_RECORD_MODE
+)
+
+
+
+
+@pytest.yield_fixture()
+def use_vcr(request, monkeypatch):
+    """
+    This fixture is applied automatically to any test using the `online` mark. It will record and playback network
+    sessions using VCR.
+    The record mode of VCR can be set using the VCR_RECORD_MODE environment variable when running tests.
+    """
+    if VCR_RECORD_MODE == 'off':
+        yield None
+    else:
+        cassette_name = ""
+        if request.module is not None:
+            cassette_name += request.module.__name__.split('tests.')[-1] + '.'
+        if request.cls is not None:
+            cassette_name += request.cls.__name__ + '.'
+        cassette_name += request.function.__name__ + '.yaml'
+
+        cassette_path = os.path.join(VCR_CASSETTE_DIR, cassette_name)
+
+        filter_query = [('applicationId', 'XXXXXX')]
+        filter_query = []
+        filter_headers = []
+        filter_post = []
+
+        online = True
+        if vcr.record_mode == 'none':
+            online = False
+        elif vcr.record_mode == 'once':
+            online = not os.path.exists(cassette_path)
+        # If we are going online, check the credentials
+        if online:
+            if os.environ.get("RAKUTEN_APP_ID", None) is None:
+                pytest.skip('need credentials to run this test')
+
+        with vcr.use_cassette(path=cassette_path,
+                              filter_query_parameters=filter_query,
+                              filter_headers=filter_headers,
+                              filter_post_data_parameters=filter_post) as cassette:
+            yield cassette
+
 def credentials():
     return {'application_id': os.environ['RAKUTEN_APP_ID']}
 
