223ba50932c6251f3b49a7f83b18adb2f735f98b
Taurus Olson <taurusolson@gmail.com>
2016-12-08 16:03:42 +0100

    Test Ichiba

---
+++ tests/webservice/test_ichiba.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import pytest
+from rakuten_ws.webservice import RakutenWebService, IchibaAPI
+from rakuten_ws.base import RakutenAPIResponse 
+
+
+# TODO Be more precise on the differences between the 2 sets of keys
+def assert_response_is_valid(response, endpoint_method):
+    valid_keys = {}
+    valid_keys['IchibaItem/Search'] = [u'count', u'hits', u'last',
+                                       u'pageCount', u'Items', u'carrier',
+                                       u'TagInformation',
+                                       u'GenreInformation', u'page',
+                                       u'first']
+    valid_keys['IchibaItem/Ranking'] = [u'lastBuildDate', u'title', u'Items']
+    valid_keys['IchibaGenre/Search'] = [u'current', u'parents', u'children']
+    valid_keys['IchibaTag/Search'] = [u'tagGroups']
+    valid_keys['IchibaProduct/Search'] = [u'count', u'hits', u'last',
+                                          u'pageCount', u'GenreInformation',
+                                          u'Products', u'page', u'first']
+    assert set(response.keys()) == set(valid_keys[endpoint_method])
+
+
+# TODO Test all the other parameters
+@pytest.mark.online
+def test_item_search(ws):
+    assert_response_is_valid(ws.ichiba.item.search(keyword="Naruto"), 'IchibaItem/Search')
+
+
+def test_item_search_pages(ws):
+    responses = ws.ichiba.item.search(keyword="Naruto").pages()
+    for i in range(1, 3):
+        response = next(responses)
+        assert response['page'] == i
+        assert_response_is_valid(response, 'IchibaItem/Search')
+
+
+@pytest.mark.online
+def test_item_ranking(ws):
+    assert_response_is_valid(ws.ichiba.item.ranking(carrier=0), 'IchibaItem/Ranking')
+
+
+def test_genre_search(ws):
+    assert_response_is_valid(ws.ichiba.genre.search(genreId=0, genrePath=1), 'IchibaGenre/Search')
+
+
+def test_tag_search(ws):
+    assert_response_is_valid(ws.ichiba.tag.search(tagId=1000943), 'IchibaTag/Search')
+
+
+def test_product_search(ws):
+    assert_response_is_valid(ws.ichiba.product.search(keyword='Naruto'),
+                             'IchibaProduct/Search')
