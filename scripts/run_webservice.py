1545197aa72ceed81be7c05304f1c87f5e20fefc
Salem Harrache <salem.harrache@abc-culture.fr>
2017-01-12 15:17:06 +0100

    Added run_webservice script helper

---
+++ scripts/run_webservice.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+import os
+
+from rakuten_ws import RakutenWebService
+
+credentials = {
+    'application_id': os.environ.get('RAKUTEN_APP_ID', ''),
+    'license_key': os.environ.get('RMS_LICENSE_KEY', ''),
+    'secret_service': os.environ.get('RMS_SECRET_SERVICE', ''),
+    'shop_url': os.environ.get('RMS_SHOP_URL', ''),
+}
+
+ws = RakutenWebService(**credentials)
