ffbd554b18e93418b155edf2037070714aaecd6a
Salem Harrache <salem.harrache@abc-culture.fr>
2017-02-01 10:24:15 +0100

    Tested R-Cabinet folder operations

--- tests/webservice/rms/test_cabinet.py
+++ tests/webservice/rms/test_cabinet.py
@@ -8,3 +8,12 @@ def test_get_usage(ws):
                      'AvailFolderCount']
     keys = list(result.keys())
     assert keys == expected_keys
+
+
+def test_folder_operations(ws):
+    test_folder_name = 'H35TX4FO6J697AX'
+    result = ws.rms.cabinet.insert_folder(folder={'folderName': test_folder_name})
+    assert result.status['systemStatus'] == "OK"
+    result = ws.rms.cabinet.get_folders()
+    folders_names = [f['FolderName'] for f in result['folders']['folder']]
+    assert test_folder_name in folders_names
